e83aefeb073bafb96832cb550340cf18bcf5e3d7
edintomato2 <156262199+edintomato2@users.noreply.github.com>
2025-04-16 23:10:33 -0400

    Starting Undo/Redo, UI Sounds, Select Box working

--- ui/new_ui.gd
+++ ui/new_ui.gd
@@ -1,23 +1,31 @@
 # UI controls placing, removing, and editing all things (triles, volumes, AOs...)
 # The meat of it all.
-
 extends Node
 
 ## -=-= UI vars. =-=-
 @onready var  _pvt: Node3D = $Pivot
 @onready var  _cam: Camera3D = $Pivot/Camera
-@onready var  _bkg: WorldEnvironment = $Background
+#@onready var  _bkg: WorldEnvironment = $Background
 @onready var _area: Area3D = $"Pivot/Area3D"
 
 var _pivot_anim = false ### Flag for if the camera's pivot is in an animation or not.
 var _total_pitch = 0.0 ### Total pitch for 3d orbit.
 
-var _select_objs: Array ### Selected objects.
+var _selected: Array ### Selected objects.
+var  _history: Array ### History. Stored as array with array of what was done (move obj, delete obj)
 
-const _mat_selected = preload("res://ui/selected/obj_selected.tres") ### "Selected" look of objects.
+const _mat_selected = preload("res://ui/selecting/obj_selected.tres") ### "Selected" look of objects.
+
+### UI Sounds
+const     _snd_ok = preload("res://ui/sounds/snd_ok.wav")
+const _snd_cancel = preload("res://ui/sounds/snd_cancel.wav")
+const     _snd_lt = preload("res://ui/sounds/snd_lt.wav")
+const     _snd_rt = preload("res://ui/sounds/snd_rt.wav")
+const     _snd_up = preload("res://ui/sounds/snd_up.wav")
+const   _snd_down = preload("res://ui/sounds/snd_down.wav")
 
 ## -=-= FEZLVL vars. =-=-
-var fzld = preload("res://main/new_saveload/fezlvl_load.gd")
+const fzld = preload("res://main/new_saveload/fezlvl_load.gd")
 var fezlvl: Dictionary
 var fezts: Array
 
@@ -26,8 +34,7 @@ func _ready() -> void:
 	_cam_setup()
 	_ui_setup()
 
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void: pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	# Camera specific code
@@ -37,21 +44,32 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	# Mouse specific code
 	_mouse_place()
+	
+	# Editor specific control
+	_editor_control()
 
 ## -=-= Editor keyboard control. =-=-
 func _editor_setup() -> void:
 	var ui_open = Shortcut.new() ## Open a FEZLVL.
 	ui_open.events = InputMap.action_get_events("editor_open")
-	$"UI/Topbar/Load File".shortcut = ui_open
+	$"UI/VBox/Topbar/Load File".shortcut = ui_open
 	
 	var ui_close = Shortcut.new() ## Closes a FEZLVL.
 	ui_close.events = InputMap.action_get_events("editor_close")
-	$"UI/Topbar/Close File".shortcut = ui_close
+	$"UI/VBox/Topbar/Close File".shortcut = ui_close
 	
 	var ui_quit = Shortcut.new() ## Quits the editor.
 	# TODO: Add "save before closing" diag box
 	ui_quit.events = InputMap.action_get_events("editor_quit")
-	$"UI/Topbar/Quit Editor".shortcut = ui_quit
+	$"UI/VBox/Topbar/Quit Editor".shortcut = ui_quit
+
+func _editor_control() -> void:
+	if Input.is_action_just_released("editor_delete", true): _editor_history("delete", _selected)
+
+func _editor_history(move: String, array: Array) -> void: # Undo/Redo history.
+	match move:
+		"delete": for obj in array: obj.hide(); _ui_sounds("snd_cancel")
+	_history.append([move, array.duplicate()])
 
 ## -=-= Camera control. =-=-
 ### Movement is modeled after Godot's editor, with some FEZification extras added.
@@ -107,17 +125,20 @@ func _cam_zoom(mouseVel: Vector2) -> void:
 			if (cam_move > 10.0) and (cam_move < 120.0): _cam.fov = cam_move
 
 func _cam_qe() -> void: ## Q + E camera rotation, like in FEZ.
+	if _pivot_anim: return
+	
 	if Input.is_action_just_pressed("cam_closest_face", true):
 		var temp = Vector3.ZERO
 		temp.y = snappedf(_pvt.rotation_degrees.y, 90)
 		_cam_tween_ctrl("rotation_degrees", temp, 0.3)
-	if Input.is_action_just_pressed("cam_lt", true): _cam_tween_ctrl("rotation_degrees", Vector3(0, -90, 0) + _pvt.rotation_degrees, 0.2)
-	if Input.is_action_just_pressed("cam_rt", true): _cam_tween_ctrl("rotation_degrees", Vector3(0, 90, 0) + _pvt.rotation_degrees, 0.2)
+		_ui_sounds("snd_ok")
+	if Input.is_action_just_pressed("cam_lt", true): _cam_tween_ctrl("rotation_degrees", Vector3(0, -90, 0) + _pvt.rotation_degrees, 0.2); _ui_sounds("snd_lt")
+	if Input.is_action_just_pressed("cam_rt", true): _cam_tween_ctrl("rotation_degrees", Vector3(0, 90, 0) + _pvt.rotation_degrees, 0.2); _ui_sounds("snd_rt")
 
 func _cam_proj_switch() -> void:
 	match _cam.projection:
-		_cam.PROJECTION_PERSPECTIVE: _cam.projection = Camera3D.PROJECTION_ORTHOGONAL
-		_cam.PROJECTION_ORTHOGONAL: _cam.projection = Camera3D.PROJECTION_PERSPECTIVE
+		_cam.PROJECTION_PERSPECTIVE: _cam.projection = Camera3D.PROJECTION_ORTHOGONAL; _ui_sounds("snd_rt")
+		_cam.PROJECTION_ORTHOGONAL: _cam.projection = Camera3D.PROJECTION_PERSPECTIVE; _ui_sounds("snd_lt")
 
 func _cam_tween_ctrl(prop: String, target: Variant, duration: float) -> void:
 	if _pivot_anim: return
@@ -140,13 +161,15 @@ func _cam_tween_ctrl(prop: String, target: Variant, duration: float) -> void:
 func _cam_wasd() -> void: ## Keyboard movement.
 	var move_target = Vector3.ZERO
 	
-	if Input.is_action_just_pressed("ui_up", true): move_target += Vector3.UP
-	if Input.is_action_just_pressed("ui_down", true): move_target += Vector3.DOWN
-	if Input.is_action_just_pressed("ui_right", true): move_target += Vector3.RIGHT
-	if Input.is_action_just_pressed("ui_left", true): move_target += Vector3.LEFT
+	if _pivot_anim: return
+	
+	if Input.is_action_just_pressed("ui_up", true): move_target += Vector3.UP; _ui_sounds("snd_up")
+	if Input.is_action_just_pressed("ui_down", true): move_target += Vector3.DOWN; _ui_sounds("snd_down")
+	if Input.is_action_just_pressed("ui_right", true): move_target += Vector3.RIGHT; _ui_sounds("snd_up")
+	if Input.is_action_just_pressed("ui_left", true): move_target += Vector3.LEFT; _ui_sounds("snd_down")
 	
 	if move_target == Vector3.ZERO: return
-	_cam_tween_ctrl("global_position", _pvt.global_position + (_pvt.global_basis * move_target), 0.15)
+	_cam_tween_ctrl("global_position", _pvt.global_position + (_pvt.global_basis * move_target), 0.1)
 
 # -=-= Mouse control. =-=-
 func _mouse_select() -> void:
@@ -193,10 +216,10 @@ func _close_fezlvl() -> void:
 ## -=-= UI control and signal links. =-=-
 func _ui_setup() -> void:
 	_ui_hide_dropdown_setup()
-	pass
+	_ui_console("Welcome to Beret!")
 
 func _ui_hide_dropdown_setup() -> void:
-	var target: MenuButton = $UI/Topbar/Hide
+	var target: MenuButton = $UI/VBox/Topbar/Hide
 	var hideables = ["Triles", "AOs", "Background Planes", "NPCs", "Volumes"]
 	
 	var shortcut = Shortcut.new()
@@ -210,7 +233,7 @@ func _ui_hide_dropdown_setup() -> void:
 		target.get_popup().add_check_item(hideables[i])
 		target.get_popup().set_item_checked(i, true)
 
-func _ui_hide_dropdown_connect( index: int) -> void:
+func _ui_hide_dropdown_connect(index: int) -> void:
 	var target: MenuButton = $UI/Topbar/Hide
 	
 	var wanna_hide = target.get_popup().get_item_text(index)
@@ -219,21 +242,34 @@ func _ui_hide_dropdown_connect( index: int) -> void:
 	var state = target.get_popup().is_item_checked(index)
 	get_tree().call_group(wanna_hide, "set_visible", state)
 
+func _ui_sounds(sound: String) -> void:
+	match sound:
+		"snd_ok":     $Sounds.set_stream(_snd_ok)
+		"snd_lt":     $Sounds.set_stream(_snd_lt)
+		"snd_rt":     $Sounds.set_stream(_snd_rt)
+		"snd_up":     $Sounds.set_stream(_snd_up)
+		"snd_down":   $Sounds.set_stream(_snd_down)
+		"snd_cancel": $Sounds.set_stream(_snd_cancel)
+	$Sounds.play()
+
 func _ui_load_file(path: String) -> void: ## Remove all objects and (re)load.
 	_close_fezlvl()
 	_load_fezlvl(path)
+	_ui_sounds("snd_ok")
 
 func _ui_load_pressed() -> void: $"UI/Popups/Load Diag".show()
 
 func _ui_close_file() -> void:
 	print("Closing level.")
 	_close_fezlvl()
+	_ui_sounds("snd_cancel")
 
 func _ui_quit_editor() -> void:
 	_close_fezlvl() ## Close safely! And try not to crash my GPU!
+	_ui_sounds("snd_cancel")
 	get_tree().quit()
 
-func _ui_selectbox_select(rect: Rect2) -> Array: # Moves the Selection Area node's bounds to where we click.
+func _ui_selectbox_changed(rect: Rect2) -> void: # Moves the Selection Area node's bounds to where we click.
 	var basis: Transform3D = _cam.global_transform
 	var start: Vector3 = _cam.project_position(rect.position, 10)
 	var end: Vector3 = _cam.project_position(rect.end, 10)
@@ -246,10 +282,18 @@ func _ui_selectbox_select(rect: Rect2) -> Array: # Moves the Selection Area node
 	### Add a very small amount to prevent Godot from complaining about a dimension equal to 0.
 	_area.scale = abs((end - start) * basis.basis) + Vector3(0.000001, 0.000001, 0.000001)
 	_area.scale.z = 10
+
+func _ui_set_dirs() -> void: _ui_sounds("snd_ok"); $UI/Popups/SetPaths.show()
+
+func _area_body_entered(body: Node3D) -> void: 
+	body.get_parent().material_overlay = _mat_selected
+	_selected.append(body.get_parent())
+
+func _area_body_exited(body: Node3D) -> void:
+	body.get_parent().material_overlay = null
+	_selected.erase(body.get_parent())
+
+func _ui_console(write: String) -> void:
+	$UI/AnimationPlayer.play("ui_console_fade")
+	$UI/VBox/Control/Console.append_text(write)
 	
-	return _area.get_overlapping_bodies()
-	#body.get_parent().material_overlay = _mat_selected
-
-func _ui_selectbox_changed(rect: Rect2) -> void: _select_objs = _ui_selectbox_select(rect)
-
-func _ui_set_dirs() -> void: $UI/Popups/SetPaths.show()
