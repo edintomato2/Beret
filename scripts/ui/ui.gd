0444bdd1779a6a2cda9fe6b842265c8b172101b5
Edin O. <22921456+edinosma@users.noreply.github.com>
2025-10-23 23:58:02 -0400

    List triles in interface

--- scripts/ui/ui.gd
+++ scripts/ui/ui.gd
@@ -56,6 +56,7 @@ func _on_file_subbuttons_pressed(id: int) -> void:
 		3:
 			loaderNode.fezlvl_close()
 			editorNode.cam_reset()
+			_tabs_clear()
 		4:
 			print("Quitting...")
 			soundNode.play_sound("cancel")
@@ -95,6 +96,8 @@ func _on_load_file_selected(path: String) -> void:
 	
 	await tween.tween_property(load_anim, "self_modulate", Color(1,1,1,0), 0.2).finished
 	$AnimationPlayer.stop()
+	
+	_tabs_triles_setup()
 
 ## Write to console
 func write_console(message: String) -> void:
@@ -110,9 +113,74 @@ func write_console(message: String) -> void:
 	tween.tween_property(console, "self_modulate", Color(1,1,1,0), 0.2).set_delay(5.0)
 
 ## Object tabs setup
-func tabs_triles_list() -> void:
+func _tabs_clear() -> void:
+	_tabs_triles_setup(true)
+
+func _tabs_triles_setup(clear: bool = false) -> void:
+	## Get list of triles in trileset, and put their textures into the ItemList
+	var palette: ItemList = $"UI Elements/VSplitContainer/Bottombar/Object Tabs/Triles"
+	if clear: palette.clear(); return
 	
-	pass
+	palette.clear()
 	
-func tabs_aos_list() -> void:
-	pass
+	var trileset = get_tree().get_first_node_in_group("TrileSet")
+	if trileset == null: return
+	
+	const TRILE_SIZE = 18
+	
+	# Textures are defined as an "Atlas Offset" multiplied by image width and height.
+	var tex: Texture2D = null
+	
+	for trile in trileset.get_children():
+		if trile is MeshInstance3D:
+			tex = trile.mesh.surface_get_material(0).albedo_texture
+			break
+	
+	for trile in trileset.get_children():
+		## Calculate texture offset
+		var offset = trile.get_meta("extras")["AtlasOffset"]
+		
+		var offsetX = floor(offset[0] * tex.get_width())
+		var offsetY = floor(offset[1] * tex.get_height())
+		var atl = AtlasTexture.new()
+		
+		## Set up texture
+		atl.atlas = tex
+		atl.region = Rect2(offsetX, offsetY, TRILE_SIZE, TRILE_SIZE)
+		atl.filter_clip = true
+		
+		## Set metadata as ID and name as tooltip.
+		var idx = palette.add_icon_item(atl, true)
+		palette.set_item_metadata(idx, trile.get_meta("extras"))
+		palette.set_item_tooltip(idx, trile.get_meta("extras")["Name"])
+	
+func _tabs_aos_setup(clear: bool = false) -> void:
+	var palette: ItemList = $"UI Elements/VSplitContainer/Bottombar/Object Tabs/AOs"
+	if clear: palette.clear(); return
+	
+	palette.clear()
+	
+	# AO textures are stored in a GLB file. Read and get them out.
+	var directory = Settings.dict["AssetDirs"][Settings.idx] + "art objects/"
+	var dir = DirAccess.open(directory)
+	dir.list_dir_begin()
+	var filename = dir.get_next()
+	while filename != "":
+		if filename.ends_with(".png"):
+			# Get AO name from file name
+			var img = Image.load_from_file(directory + filename) # Load texture
+			var tex = ImageTexture.create_from_image(img)
+			var atlas = AtlasTexture.new()
+			var aoname: String = filename.to_upper().get_basename().get_basename()
+			
+			atlas.atlas = tex
+			@warning_ignore("integer_division")
+			atlas.region = Rect2(0, 0, img.get_width() / 6, img.get_height())
+			atlas.filter_clip = true
+			
+			var iconIdx = palette.add_icon_item(atlas, true)
+			
+			palette.set_item_tooltip(iconIdx, aoname) # Set name as tooltip.
+			palette.set_item_metadata(iconIdx, aoname)
+			
+		filename = dir.get_next()
