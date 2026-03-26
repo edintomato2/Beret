01e28ab18dfe7dee2168d32419635dbfa3d18698
Edin O. <22921456+edinosma@users.noreply.github.com>
2026-03-26 17:04:05 -0400

    Publish the final release

--- scripts/ui/popups/file_diags.gd
+++ scripts/ui/popups/file_diags.gd
@@ -3,5 +3,5 @@ extends FileDialog
 @export var RootDir: String = ""
 
 func _ready() -> void:
-	if Settings.dict["AssetDirs"][Settings.idx] != null:
+	if ("AssetDirs" in Settings) and (Settings.dict["AssetDirs"][Settings.idx] != null):
 		self.root_subfolder = Settings.dict["AssetDirs"][Settings.idx] + RootDir
